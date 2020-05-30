def64c0cac2018243729fb865432e0f04b13cf9f
fishwickid <chadfishwick@gmail.com>
2020-05-30 20:43:36 +0800

    added password to display in text box

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-//variables
+//Create arrays with all the characters in the password
 
 
 var userChoice = [];
@@ -8,14 +8,19 @@ var upperCase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
 var numbers=[0,1,2,3,4,5,6,7,8,9,]
 var specialChar = ['!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','^']
 
-// Prompts for User after they click button
+// Series of prompt quesstions for User after they click button
 
 function promptMe() {
 
+// Ask the user how many characeters they want in their password
 
 var passwordLength = prompt("How many numbers do you want your password to contain? (8 - 128)");
 console.log(passwordLength);
+
+// Turn password into a number from a string value
 passwordLength = parseFloat(passwordLength);
+
+// Create alert if the user enters a value less that 8 or more than 128
 if (passwordLength < 8  || passwordLength > 128) {
   alert("Your password needs to be at least 8 characters and no more than 128");
   return
@@ -24,51 +29,56 @@ else {
   alert ("Your password is going to be " + passwordLength + " characters");
 }
 
-
-
-console.log(passwordLength);
-
+// Ask user if they want uppercase in their password
 
 var upperCaseChoice = confirm("Do you wish to use uppercase?");
 console.log(upperCaseChoice);
  if (upperCaseChoice) {
+//Concatinate the predetermined array to include uppercase letters (this happens for every prompt)
    userChoice = userChoice.concat(upperCase);
    console.log(userChoice);
    //console.log(userChoice.length);
  }
+
+// Ask user if they want lowercase
 
 var lowerCaseChoice = confirm("Do you wish to use lowercase?");
 console.log(lowerCaseChoice);
 if (lowerCaseChoice) {
   userChoice = userChoice.concat(lowerCase);
    console.log(userChoice);
-   //console.log(userChoice.length);
-   
+ 
 }
+
+//Ask user if they want numbers + concatination
 
 var numbersChoice = confirm("Do you wish to use numbers?")
 console.log(numbersChoice);
 if (numbersChoice) {
   userChoice = userChoice.concat(numbers);
   console.log(userChoice);
-  //console.log(userChoice.length);
 }
+
+// Ask user if they want special characters + concatinate
 
 var specialCharactersChoice = confirm("Do you wish to use special character?")
 console.log(specialCharactersChoice);
 if (specialCharactersChoice) {
   userChoice = userChoice.concat(specialChar);
 
-  console.log(userChoice);
-  console.log(passwordLength);
 }
 
+// create empty array to fill with random outputted numbers
   var charactersArray = [];
+
+// create loop to keep adding random numbers to the array until it reaches the limit of
+// how many numbers the user wanted for their password.
 
   for(var i = 0; i < passwordLength;i++) {
     
     var randomCharacter = Math.floor(Math.random() * userChoice.length);
     charactersArray += userChoice[randomCharacter];
   }
-  alert("Your new password is " + charactersArray);
+  document.getElementById("password").innerHTML=charactersArray;
+  alert("Your new password is: " + charactersArray);
 }
